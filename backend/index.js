f39f09ceb53cfab320640671492ad3cc2a1c141c
David Runion <david.runion@imedemr.com>
2024-02-21 23:53:43 -0500

    indexer v2

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,22 @@
+/*
+    For each block:
+    - Iterate through transactions, filter for appcall transactions:
+    - if newly created app - check if arc72, and if so add to collections table
+        - set mintRound and totalSupply
+    
+    - else if appid is NOT in the collections table continue
+
+    - update collection data:
+        - get transaction history since lastSyncRound
+        - for each transaction, set token owner = transaction toAddr
+        - for each transaction, if fromAddr == zeroAddress
+            - set token mintRound, metadataURI, metadata
+        - insertTransaction for token ({transactionId, contractId, tokenId, round, fromAddr: from, toAddr: to, timestamp})
+
+        - get approval history since lastSyncRound
+        - for each approval, set token approved = approvedAddr
+*/
+
 import { arc72 as Contract } from "ulujs";
 import { isARC72, zeroAddress, algodClient, indexerClient, sleep, output } from "./utils.js";
 import Database from "./database.js";
@@ -5,114 +24,137 @@ import Database from "./database.js";
 const db = new Database('./db.sqlite');
 
 // get last sync round from info table
-let last_block = Number((await db.getInfo("syncRound"))?.value??0);
+let last_block = Number((await db.getInfo("syncRound"))?.value ?? 0);
 let end_block = (await algodClient.status().do())['last-round'];
 console.log(`Database Synced to round: ${last_block}. Current round: ${end_block}`);
 
-while(true) {
-  if (last_block >= end_block) {
-      output(`Reached end of chain, sleeping for 10 seconds...`, true);
-      await sleep(10000);
-      try {
-          end_block = (await algodClient.status().do())['last-round'];
-      }
-      catch (error) {
-          output(`Error retrieving end block from API: ${error.message}, retrying.`, true);
-          await sleep(10000); // wait 10 seconds before trying again
-      }
-      continue;
-  }
-  let i = last_block + 1;
+while (true) {
+    if (last_block >= end_block) {
+        output(`Reached end of chain, sleeping for 3 seconds...`, true);
+        await sleep(3000);
+        try {
+            end_block = (await algodClient.status().do())['last-round'];
+        }
+        catch (error) {
+            output(`Error retrieving end block from API: ${error.message}, retrying.`, true);
+            await sleep(10000); // wait 10 seconds before trying again
+        }
+        continue;
+    }
+    let i = last_block + 1;
 
-  output(`Retrieving block ${i} (${end_block - i} behind)`, true);
+    output(`Retrieving block ${i} (${end_block - i} behind)`, true);
 
-  try {
-    const timeoutPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject(new Error('Request timed out'));
-        }, 5000); // 5 second timeout
-    });
+    try {
+        const timeoutPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(new Error('Request timed out'));
+            }, 5000); // 5 second timeout
+        });
 
-    const blk = await Promise.race([algodClient.block(i).do(), timeoutPromise]);
-    const rnd = blk.block.rnd;
+        const blk = await Promise.race([algodClient.block(i).do(), timeoutPromise]);
+        const rnd = blk.block.rnd;
 
-    // get all application transactions from block
-    if (blk.block.txns) {
-      const appTxns = blk.block.txns.filter((txn) => typeof txn.apid !== 'undefined' || typeof txn.txn?.apid !== 'undefined');
+        // get all application transactions from block
+        if (blk.block.txns) {
+            const appTxns = blk.block.txns.filter((txn) => typeof txn.apid !== 'undefined' || typeof txn.txn?.apid !== 'undefined');
 
-      // for each txn in txns
-      for (const txn of appTxns) {
-        const contractId = txn.apid ?? txn.txn.apid;
+            // for each txn in txns
+            for (const txn of appTxns) {
+                const contractId = txn.apid ?? txn.txn.apid;
+                let lastSyncRound, ctc;
 
-        const ctc = new Contract(contractId, algodClient, indexerClient);
-        const isArc72 = await isARC72(ctc);
-        
-        if (isArc72) {
-          output(`\nFound ARC72 contract AppCall at ${contractId}, in block ${rnd}`);
+                // if txn is a contract creation, check if it's an ARC72 contract
+                if (txn.txn.apap && txn.txn.apsu) {
+                    ctc = new Contract(contractId, algodClient, indexerClient);
 
-          let lastSyncRound = await db.getCollectionLastSync(contractId);
-          if (rnd < lastSyncRound) lastSyncRound = rnd;
+                    if (await isARC72(ctc)) {
+                        console.log(`Adding new contract ${contractId} to collections table`);
+                        const mintRound = rnd;
+                        const totalSupply = (await ctc.arc72_totalSupply()).returnValue;
+                        lastSyncRound = rnd;
+                        await db.insertOrUpdateCollection({ contractId, totalSupply, createRound: mintRound, lastSyncRound });
+                    }
+                    else {
+                        console.log(`Contract ${contractId} is not an ARC72 contract, skipping`);
+                        continue;
+                    }
+                }
+                else {
+                    // check if we have this contractId in the database
+                    lastSyncRound = await db.getCollectionLastSync(contractId);
+                    if (lastSyncRound == 0) {
+                        console.log(`Contract ${contractId} not found in collections table, skipping`);
+                        continue;
+                    }
 
-          const totalSupply = (await ctc.arc72_totalSupply()).returnValue;
+                    console.log(`Updating contract ${contractId} in collections table`);
+                    ctc = new Contract(contractId, algodClient, indexerClient);
+                }
 
-          await db.insertOrUpdateCollection({contractId, totalSupply, createRound: rnd, lastSyncRound: rnd});
+                if (lastSyncRound < rnd) {
+                    // get transaction history since lastSyncRound
+                    const events = await ctc.arc72_Transfer({ minRound: (lastSyncRound+1), maxRound: rnd });
 
-          // update tokens table for all tokens from index 0 to totalSupply
-          output(`\nUpdating collection ${contractId} tokens from index 0 to ${totalSupply}...`);
-          for(let i = 0; i < totalSupply; i++) {
-            try {
-              const tokenId = (await ctc.arc72_tokenByIndex(i)).returnValue;
-              const owner = (await ctc.arc72_ownerOf(tokenId)).returnValue;
-              const approved = (await ctc.arc72_getApproved(tokenId)).returnValue;
-              const metadataURI = (await ctc.arc72_tokenURI(tokenId)).returnValue;
-              const metadata = JSON.stringify(await fetch(metadataURI).then((res) => res.json()));
+                    console.log(`Processing ${events.length} events for contract ${contractId} from round ${lastSyncRound} to ${rnd}`);
 
-              await db.insertOrUpdateToken({contractId, tokenId, tokenIndex: i, owner, metadataURI, metadata, approved});
+                    // for each event, record a transaction in the database
+                    for await (const event of events) {
+                        const [transactionId, round, timestamp, from, to, tokenId] = event;
+
+                        if (from == zeroAddress) {
+                            // new token mint
+                            const metadataURI = (await ctc.arc72_tokenURI(tokenId)).returnValue;
+                            const metadata = JSON.stringify(await fetch(metadataURI).then((res) => res.json()));
+                            const totalSupply = (await ctc.arc72_totalSupply()).returnValue;
+
+                            await db.insertOrUpdateToken({ contractId, tokenId, tokenIndex: 0, owner: to, metadataURI, metadata, approved: zeroAddress, mintRound: round});
+                            await db.updateCollectionTotalSupply(contractId, totalSupply);
+
+                            console.log(`Minted token ${tokenId} for contract ${contractId}`);
+                        }
+                        else {
+                            await db.updateTokenOwner(contractId, tokenId, to);
+                            console.log(`Updated token ${tokenId} owner to ${to}`);
+                        }
+
+                        await db.insertTransaction({ transactionId, contractId, tokenId, round, fromAddr: from, toAddr: to, timestamp });
+
+                    }
+                
+                    // get approval history since lastSyncRound
+                    const aevents = await ctc.arc72_Approval({ minRound: (lastSyncRound+1), maxRound: rnd });
+
+                    console.log(`Processing ${aevents.length} approval events for contract ${contractId} from round ${lastSyncRound} to ${rnd}`);
+
+                    // for each event, record a transaction in the database
+                    for await (const event of aevents) {
+                        const [transactionId, round, timestamp, from, to, tokenId] = event;
+
+                        await db.updateTokenApproved(contractId, tokenId, to);
+                        console.log(`Updated token ${tokenId} approved to ${to}`);
+                    }
+
+                    // update lastSyncRound in collections table
+                    await db.updateCollectionLastSync(contractId, rnd);
+                    console.log(`Updated lastSyncRound for contract ${contractId} to ${rnd}`);
+                }
+                else {
+                    console.log(`No new events for contract ${contractId} since lastSyncRound ${lastSyncRound}`);
+                }
             }
-            catch(err) {
-              console.log(err);
-              continue;
-            }
-          }
-
-          output(`\nUpdating collection ${contractId} transactions from round ${lastSyncRound} to ${rnd}...`);
-
-          // get events since lastSyncRound
-          // [ transactionId: string, round: number, timestamp: number, from: string, to: string, tokenId: number ]
-          const events = await ctc.arc72_Transfer({ minRound: lastSyncRound, maxRound: rnd });
-
-          // for each event, record a transaction in the database
-          for await (const event of events) {
-            const [transactionId, round, timestamp, from, to, tokenId] = event;
-            await db.insertTransaction({transactionId, contractId, tokenId, round, fromAddr: from, toAddr: to, timestamp});
-            //await db.updateTokenOwner(contractId, tokenId, to);
-
-            // if from == zeroAddress, then this is a mint transaction. update tokens table with mintRound
-            if (from === zeroAddress) {
-              await db.updateTokenMintRound(contractId, tokenId, round);
-            }
-            if (round > lastSyncRound) lastSyncRound = round;
-          }
-
-          output('Done.\n');
-
-          // set lastSyncRound in collections table
-          await db.updateCollectionLastSync(contractId, lastSyncRound);
 
         }
-      }
-      
+    } catch (error) {
+        if (error.message === 'Request timed out') {
+            output(`Error retrieving block ${i} from API: request timed out, retrying.`, true);
+        } else {
+            output(`Error retrieving block ${i} from API: ${error.message}, retrying.`, true);
+        }
+        await sleep(10000); // wait 10 seconds before trying again
+        continue;
     }
-  } catch (error) {
-    if (error.message === 'Request timed out') {
-      output(`Error retrieving block ${i} from API: request timed out, retrying.`, true);
-    } else {
-      output(`Error retrieving block ${i} from API: ${error.message}, retrying.`, true);
-    }
-    await sleep(10000); // wait 10 seconds before trying again
-    continue;
-  }
 
-  last_block = i;
-  await db.setInfo("syncRound", last_block);
+    last_block = i;
+    await db.setInfo("syncRound", last_block);
 }
---
+++ backend/index_v1.js
@@ -0,0 +1,157 @@
+/* TODO
+  - Listen only for new contract creation and check if ARC72
+  - Listen only for appCalls of ARC72 contracts that have already been added to database
+
+  - When see a transfer from Zero address, add the token to the database and pull the metadata
+  - 
+
+*/
+import { arc72 as Contract } from "ulujs";
+import { isARC72, zeroAddress, algodClient, indexerClient, sleep, output } from "./utils.js";
+import Database from "./database.js";
+
+const db = new Database('./db.sqlite');
+
+// get last sync round from info table
+let last_block = Number((await db.getInfo("syncRound"))?.value??0);
+let end_block = (await algodClient.status().do())['last-round'];
+console.log(`Database Synced to round: ${last_block}. Current round: ${end_block}`);
+
+last_block = 4419072;
+
+// update collection (async to allow multithread). if fails, reset syncRound to rnd
+// if contract is null it's a new contract (not in database yet)
+const updateCollection = async (contractId, rnd, contract) => {
+  try {
+    const ctc = new Contract(contractId, algodClient, indexerClient);
+    const isArc72 = await isARC72(ctc);
+    
+    if (isArc72) {
+      output(`\nFound ARC72 contract AppCall at ${contractId}, in block ${rnd}`);
+
+      let lastSyncRound = await db.getCollectionLastSync(contractId);
+      if (rnd < lastSyncRound) lastSyncRound = rnd;
+
+      // if this is a new contract
+      if (!contract) {
+        output(`\nAdding new collection ${contractId}...`);
+        await db.insertOrUpdateCollection({contractId, createRound: rnd, lastSyncRound: rnd});
+      }
+
+      const totalSupply = (await ctc.arc72_totalSupply()).returnValue;
+
+      await db.insertOrUpdateCollection({contractId, totalSupply, createRound: rnd, lastSyncRound: rnd});
+
+      // update tokens table for all tokens from index 0 to totalSupply
+      output(`\nUpdating collection ${contractId} tokens from index 0 to ${totalSupply}...`);
+      for(let i = 0; i < totalSupply; i++) {
+        try {
+          const tokenId = (await ctc.arc72_tokenByIndex(i)).returnValue;
+          const owner = (await ctc.arc72_ownerOf(tokenId)).returnValue;
+          const approved = (await ctc.arc72_getApproved(tokenId)).returnValue;
+          const metadataURI = (await ctc.arc72_tokenURI(tokenId)).returnValue;
+          const metadata = JSON.stringify(await fetch(metadataURI).then((res) => res.json()));
+
+          await db.insertOrUpdateToken({contractId, tokenId, tokenIndex: i, owner, metadataURI, metadata, approved});
+        }
+        catch(err) {
+          console.log(err);
+          continue;
+        }
+      }
+
+      output(`\nUpdating collection ${contractId} transactions from round ${lastSyncRound} to ${rnd}...`);
+
+      // get events since lastSyncRound
+      // [ transactionId: string, round: number, timestamp: number, from: string, to: string, tokenId: number ]
+      const events = await ctc.arc72_Transfer({ minRound: lastSyncRound, maxRound: rnd });
+
+      // for each event, record a transaction in the database
+      for await (const event of events) {
+        const [transactionId, round, timestamp, from, to, tokenId] = event;
+        await db.insertTransaction({transactionId, contractId, tokenId, round, fromAddr: from, toAddr: to, timestamp});
+        //await db.updateTokenOwner(contractId, tokenId, to);
+
+        // if from == zeroAddress, then this is a mint transaction. update tokens table with mintRound
+        if (from === zeroAddress) {
+          await db.updateTokenMintRound(contractId, tokenId, round);
+        }
+        if (round > lastSyncRound) lastSyncRound = round;
+      }
+
+      output('Done.\n');
+
+      // set lastSyncRound in collections table
+      await db.updateCollectionLastSync(contractId, lastSyncRound);
+
+    }
+  }
+  catch(err) {
+    console.log(err);
+    await db.setInfo("syncRound", rnd);
+  }
+}
+
+while(true) {
+  if (last_block >= end_block) {
+      output(`Reached end of chain, sleeping for 10 seconds...`, true);
+      await sleep(10000);
+      try {
+          end_block = (await algodClient.status().do())['last-round'];
+      }
+      catch (error) {
+          output(`Error retrieving end block from API: ${error.message}, retrying.`, true);
+          await sleep(10000); // wait 10 seconds before trying again
+      }
+      continue;
+  }
+  let i = last_block + 1;
+
+  output(`Retrieving block ${i} (${end_block - i} behind)`, true);
+
+  try {
+    const timeoutPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error('Request timed out'));
+        }, 5000); // 5 second timeout
+    });
+
+    const blk = await Promise.race([algodClient.block(i).do(), timeoutPromise]);
+    const rnd = blk.block.rnd;
+
+    // get all application transactions from block
+    if (blk.block.txns) {
+      const appTxns = blk.block.txns.filter((txn) => typeof txn.apid !== 'undefined' || typeof txn.txn?.apid !== 'undefined');
+
+      // for each txn in txns
+      for (const txn of appTxns) {
+        const contractId = txn.apid ?? txn.txn.apid;
+
+        // if txn is a contract creation, check if it's an ARC72 contract
+        if (txn.apap && txn.apsu) {
+          updateCollection(contractId, rnd, null);
+        }
+        else {
+          // check if we have this contractId in the database
+          const contract = await db.getContract(contractId);
+
+          if (contract) {
+            await updateCollection(contractId, rnd, contract);
+          }
+        }
+      }
+      
+    }
+  } catch (error) {
+    if (error.message === 'Request timed out') {
+      output(`Error retrieving block ${i} from API: request timed out, retrying.`, true);
+    } else {
+      output(`Error retrieving block ${i} from API: ${error.message}, retrying.`, true);
+    }
+    await sleep(10000); // wait 10 seconds before trying again
+    continue;
+  }
+
+  last_block = i;
+  await db.setInfo("syncRound", last_block);
+}
